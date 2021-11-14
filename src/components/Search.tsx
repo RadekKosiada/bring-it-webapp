051e0d695054d4faf2b84b97f2d36c163dde263c
RadekKosiada <kosiadar@gmail.com>
2021-11-14 15:47:56 +0100

    Adjust formatting in Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -65,10 +65,10 @@ function Search() {
                 return (
                     <div key={index}>
                         <p>{product.name}</p>
-                        <img 
-                        src={product.image ? product.image: 
-                        'https://via.placeholder.com/50'}
-                        alt={`image of ${product.name}`}
+                        <img
+                            src={product.image ? product.image :
+                                'https://via.placeholder.com/50'}
+                            alt={`image of ${product.name}`}
                         />
                     </div>
 
