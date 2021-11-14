67397f8ef2405718df50a4887405332e98c430ac
RadekKosiada <kosiadar@gmail.com>
2021-11-14 14:15:43 +0100

    Add Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,12 @@
+import '../App.css';
+import { fetchProducts } from '../api/index';
+
+function Search() {
+  return (
+    <div >
+      <p>Search</p>
+    </div>
+  );
+}
+
+export default Search;
