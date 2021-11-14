89a76449f896599fe15317590997754813bcd4cf
RadekKosiada <kosiadar@gmail.com>
2021-11-14 14:25:26 +0100

    Add input to Search & onChange()

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,37 @@
 import '../App.css';
+import React, {useState, useCallback} from 'react';
 import { fetchProducts } from '../api/index';
 
 function Search() {
-  return (
-    <div >
-      <p>Search</p>
-    </div>
-  );
+    const [value, setValue] = useState('')
+
+
+    const handleChange = useCallback (
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            console.log(value)
+            setValue(e.target.value);
+        },
+        [value],
+    );
+
+    return (
+        <div>
+            <form
+            // onSubmit={}
+            >
+                <label htmlFor="">
+                    <input
+                        placeholder="Suche"
+                        id=""
+                        type="text"
+                        value={value}
+                        onChange={handleChange}
+                    />
+                </label>
+            </form>
+
+        </div>
+    );
 }
 
 export default Search;
