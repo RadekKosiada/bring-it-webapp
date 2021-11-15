f09491c6ba0f8ba934bf3fd623b6d288dda8bb66
RadekKosiada <kosiadar@gmail.com>
2021-11-15 20:47:16 +0100

    Add CSS classes to Results; Render basePrice & baseUnit conditionally

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -6,23 +6,30 @@ import Products from '../interfaces/Products';
 
 export default function Results({ products }: { products: Array<Products> }) {
     return (
-        <div>
-            <p>Ergebnisse: {products.length}</p>
+        <div className="results-container">
+            <p className="results-sum">Ergebnisse: {products.length}</p>
             {products && products.map((product) => {
                 return (
-                    <div key={product.sku}>
-                        <p>{product.name}</p>
-                        <p>{`${formatPrice(product.price, 2)} €`}</p>
-                        <p>{product.packing}</p>
-                        <p>
-                            {product.basePrice && <span>{product.basePrice} / </span>}
-                            {product.baseUnit && <span>{product.baseUnit}</span>}
-                        </p>
+                    <div
+                        className="product-container"
+                        key={product.sku}>
+
                         <img
                             src={product.image ? product.image :
                                 'https://via.placeholder.com/50'}
                             alt={`image of ${product.name}`}
                         />
+                        <div className="product-details">
+                            <p>{product.name}</p>
+                            <p>{`${formatPrice(product.price, 2)} €`}</p>
+                            <p>{product.packing}</p>
+                            <p>
+                                {product.basePrice > 0 && <span>{product.basePrice}€ / </span>}
+                                {product.baseUnit !== '0' && <span>{product.baseUnit}</span>}
+                            </p>
+                
+                        </div>
+                        <hr />
                     </div>
 
                 )
