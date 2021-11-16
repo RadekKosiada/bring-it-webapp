de5c8b6f47e3188cacca54fc806d59072b14c4eb
RadekKosiada <kosiadar@gmail.com>
2021-11-16 22:22:13 +0100

    Define type for props in QueryButton

--- src/components/QueryButton.tsx
+++ src/components/QueryButton.tsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-// type QueryButtonProps = {
-//     title: string,
-//     getQuery: (feedback: string) =>  void
-// }
+type QueryButtonProps = {
+    title: string,
+    getQuery: (feedback: string) =>  void
+}
 
-function QueryButton({ title, getQuery }: { title: string, getQuery: (feedback: string) =>  void}) {
+function QueryButton({ title, getQuery }: { title: string, getQuery: QueryButtonProps['getQuery']}) {
 
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
