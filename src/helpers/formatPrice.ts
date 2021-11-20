132838173427819c0c4404458eaf46934511e9c7
RadekKosiada <kosiadar@gmail.com>
2021-11-20 20:56:23 +0100

    Rewrite format price and comment its code

--- src/helpers/formatPrice.ts
+++ src/helpers/formatPrice.ts
@@ -1,6 +1,17 @@
-function formatPrice(numberToRound: number, decimalPlaces: number ) {
-    const priceToString = numberToRound.toString();
-    return Number.parseFloat(priceToString).toFixed(decimalPlaces);
+/**
+ * Add exponential Notation and creates a string
+ * Changes the given string into the number, with the decimal point moved to the right by two
+ * Rounds the given number
+ * Adds again Exponential Notation and thus changes the number into a string 
+ * Changes the string into the result number with decimal point moved back to the left by two and thus gives final result
+ * @param numberToRound 
+ * @param decimalPlaces 
+ * @returns 
+ */
+ function formatPrice(numberToRound: number, decimalPlaces: number): number {
+
+    return Number(Math.round(Number(numberToRound + "e+" + decimalPlaces)) + "e-" + decimalPlaces); 
+
 }
 
 export default formatPrice;
