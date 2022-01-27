26f72f8fe2fb2ff6f725d4bfe172441bd9355cc6
RadekKosiada <kosiadar@gmail.com>
2022-01-27 17:47:01 +0100

    Use input.value instead of button.innerHTML due to possible XSS attack

--- src/components/QueryButton.tsx
+++ src/components/QueryButton.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 
 type QueryButtonProps = {
     title: string,
-    getQuery: (feedback: string) =>  void
+    getQuery: (feedback: string) => void
 }
 
-function QueryButton({ title, getQuery }: { title: string, getQuery: QueryButtonProps['getQuery']}) {
+function QueryButton({ title, getQuery }: { title: string, getQuery: QueryButtonProps['getQuery'] }) {
 
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
         e.preventDefault();
-        getQuery((e.target as HTMLButtonElement).innerHTML);
+        getQuery((e.target as HTMLInputElement).value);
     }
 
     return (
-        <button className="query-button" onClick={handleClick}>{title}</button>
+        <input className="query-button" type="button" value={title} onClick={handleClick} />
     )
 }
 
 export default QueryButton;
+
 
