b3a2da4ba604d609a0b7d4c0feef385962c6229e
RadekKosiada <kosiadar@gmail.com>
2022-01-28 16:58:29 +0100

    Add a loading icon from font awesome

--- src/components/LoadingSign.tsx
+++ src/components/LoadingSign.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+
 function LoadingSign() {
 
   return (
-    <p className="loading-sign">LOADING...</p>
+    <div>
+      <FontAwesomeIcon className="loading-sign" icon={faSpinner} />
+    </div>
   )
 }
 
